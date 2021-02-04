da96110785e6fee635c44190fcad789b41705fbc
Andrey Nemilov (external) <Andrey_Nemilov@swissre.com>
2021-02-04 14:39:16 +0300

    test

--- main.tf
+++ main.tf
@@ -18,16 +18,16 @@ resource "azurerm_resource_group" "rg" {
 }
 
 # Create virtual network
-resource "azurerm_virtual_network" "myterraformnetwork" {
-    name                = "myVnet"
-    address_space       = ["10.0.0.0/16"]
-    location            = "eastus"
-    resource_group_name = azurerm_resource_group.myterraformgroup.name
+# resource "azurerm_virtual_network" "myterraformnetwork" {
+#     name                = "myVnet"
+#     address_space       = ["10.0.0.0/16"]
+#     location            = "eastus"
+#     resource_group_name = azurerm_resource_group.myterraformgroup.name
 
-    tags = {
-        environment = "Terraform Demo"
-    }
-}
+#     tags = {
+#         environment = "Terraform Demo"
+#     }
+# }
 
 # # Create subnet
 # resource "azurerm_subnet" "myterraformsubnet" {
