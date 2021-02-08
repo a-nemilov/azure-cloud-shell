e55ef2a68df11d644da48fe52782c91a9c988ba3
Andrey Nemilov (external) <Andrey_Nemilov@swissre.com>
2021-02-08 23:42:35 +0300

    test

--- main.tf
+++ main.tf
@@ -15,7 +15,7 @@ provider "azurerm" {
 terraform {
   backend "azurerm" {
     resource_group_name  = "rg-terraformstate"
-    storage_account_name = "myterrastatestoragestatetest"
+    storage_account_name = "myterrastatestoragetest"
     container_name       = "terraformdemo"
     key                  = "dev.terraform.tfstate"
   }
