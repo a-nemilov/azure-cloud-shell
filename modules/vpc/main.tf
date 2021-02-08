0c06b32c87fd8b9c1345792dd41ad3df6e123a05
Andrey Nemilov (external) <Andrey_Nemilov@swissre.com>
2021-02-08 23:31:29 +0300

    clear code

--- modules/vpc/main.tf
+++ modules/vpc/main.tf
@@ -22,28 +22,3 @@ resource "azurerm_subnet" "subnet" {
   virtual_network_name = azurerm_virtual_network.virtual_network.name
   address_prefixes     = ["10.0.1.0/24"]
 }
-
-
-
-# resource "azurerm_virtual_network" "my-vn-1" {
-#   name                = "example-vnet"
-#   address_space       = ["10.0.0.0/16"]
-#   location            = azurerm_resource_group.my-rg-1.location
-#   resource_group_name = azurerm_resource_group.my-rg-1.name
-# }
-
-# resource "azurerm_subnet" "example" {
-#   name                 = "example-subnet"
-#   resource_group_name  = azurerm_resource_group.my-rg-1.name
-#   virtual_network_name = azurerm_virtual_network.my-vn-1.name
-#   address_prefixes     = ["10.0.1.0/24"]
-
-#   delegation {
-#     name = "delegation"
-
-#     service_delegation {
-#       name    = "Microsoft.ContainerInstance/containerGroups"
-#       actions = ["Microsoft.Network/virtualNetworks/subnets/join/action", "Microsoft.Network/virtualNetworks/subnets/prepareNetworkPolicies/action"]
-#     }
-#   }
-# }
