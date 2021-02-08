f14cef7af1e02c3b75ced0f82675f7373014a656
Andrey Nemilov (external) <Andrey_Nemilov@swissre.com>
2021-02-08 17:02:27 +0300

    fix

--- modules/vpc/vpc_main.tf
+++ modules/vpc/vpc_main.tf
@@ -11,7 +11,7 @@ resource "azurerm_resource_group" "main_resource_group" {
 
 resource "azurerm_virtual_network" "virtual_network" {
   name                = var.virtual_network_name
-  address_space       = [var.virtual_network_address_space]
+  address_space       = ["10.0.0.0/16"]
   location            = azurerm_resource_group.main_resource_group.location
   resource_group_name = azurerm_resource_group.main_resource_group.name
 }
@@ -20,5 +20,5 @@ resource "azurerm_subnet" "subnet" {
   name                 = var.subnet_name
   resource_group_name  = azurerm_resource_group.main_resource_group.name
   virtual_network_name = azurerm_virtual_network.virtual_network.name
-  address_prefixes     = [var.subnet_address_prefixes]
+  address_prefixes     = ["10.0.2.0/24"]
 }
