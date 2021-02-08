44e5ae16d4f4722ebfedd1c94c650b8441c4dea2
Andrey Nemilov (external) <Andrey_Nemilov@swissre.com>
2021-02-08 17:04:39 +0300

    fix

--- modules/vpc/vpc_variables.tf
+++ modules/vpc/vpc_variables.tf
@@ -16,14 +16,14 @@ variable subnet_name {
   type        = string
   description = "Plaese enter subnet name"
 }
-variable virtual_network_address_space {
-  default     = ["10.0.0.0/16"]
-  description = "Enter virtual network address space"
-}
-variable subnet_address_prefixes {
-  default     = ["10.0.2.0/24"]
-  description = "Subnet adress prefixes"
-}
+# variable virtual_network_address_space {
+#   default     = ["10.0.0.0/16"]
+#   description = "Enter virtual network address space"
+# }
+# variable subnet_address_prefixes {
+#   default     = ["10.0.2.0/24"]
+#   description = "Subnet adress prefixes"
+# }
 
 
 
